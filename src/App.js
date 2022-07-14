709eac19a1f855061cb7173f61a7f4350bdc227b
Clara - Maria Ciarnau <ciarnau.clara@gmail.com>
2022-07-14 12:06:11 +0300

    Init deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import profileImage from './img/Poza_CV.jpg';
+import './style/App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={profileImage} alt={"profile-image"} className="profile_image"/>
+        <h1>Clara - Maria Ciarnău</h1>
+        <div>something</div>
       </header>
     </div>
   );
